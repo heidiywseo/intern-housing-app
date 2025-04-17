beb87219a1e6ec3c173ad83cf34a823fc81b84ac
heidiywseo <ywseo@seas.upenn.edu>
2025-04-17 16:16:28 -0400

    IN PROGRESS: initial listings api route

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('./config.json');
+
+const app = express();
+app.use(cors({
+  origin: '*',
+}));
+app.use(express.json());
+
+const listingsRoutes = require('./routes/listings');
+app.use('/listings', listingsRoutes);
+
+app.listen(config.server_port, () => {
+  console.log(`Server running at http://${config.server_host}:${config.server_port}/`);
+});
+
+module.exports = app;
